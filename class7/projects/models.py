34326f1a957bf48006b1d636a2b63278f7f89d9d
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-04-27 21:44:22 +0500

    class 7

---
+++ class7/projects/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from students.models import Student  # Cross-app import
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    lead_student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ class7/students/admin.py
@@ -0,0 +1,8 @@
+# students/admin.py
+from django.contrib import admin
+
+# 1. Import your model
+from .models import Student
+
+# 2. Register it with the admin site
+admin.site.register(Student)
